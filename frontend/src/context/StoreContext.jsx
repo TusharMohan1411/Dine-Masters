3c46198b3316173453fb1ec93d84522d4c5baf0b
Tushar <tusharmohan0416@gmail.com>
2024-10-23 07:05:10 +0530

    errors fixed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://dine-masters-backend.onrender.com/" || "http://localhost:4000"
+    const url = "https://dine-masters-backend.onrender.com" || "http://localhost:4000"
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
